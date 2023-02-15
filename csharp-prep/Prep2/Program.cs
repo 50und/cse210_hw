72a7a93a962e13f4814f42606915674f016b262e
StephenC <col21040@byui.edu>
2023-02-14 20:54:24 -0700

    finished first two prep assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is the grade?");
+        int grade = Int32.Parse(System.Console.ReadLine());
+        string letter = "";
+        if (grade>=90)
+        {
+            letter = "A";
+        }
+        else if (grade>=80)
+        {
+            letter = "B";
+        }
+        else if (grade>=70)
+        {
+            letter = "C";
+        }
+        else if (grade>=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        string plusMinus = "";
+        if (grade % 10 >= 7)
+        {
+            plusMinus = "+";
+        }
+        else if (grade % 10 <=3)
+        {
+            plusMinus = "-";
+        }
+        else
+        {
+            plusMinus = "";
+        }
+        System.Console.WriteLine($"Student's Grade is {letter}{plusMinus}");
     }
 }
